bb5410119a831b37953334b336a3de8a1e332f96
Shaojun Bao <youngwolf111@gmail.com>
2023-09-08 15:10:52 -0400

    finished order details component

--- src/components/OrderDetail/OrderDetail.jsx
+++ src/components/OrderDetail/OrderDetail.jsx
@@ -1,29 +1,37 @@
 import './OrderDetail.css';
 
-export default function OrderDetail({ orderItems }) {
-  const totalAmount = orderItems.reduce((acc, item) => acc + item.price, 0).toFixed(2);
+export default function OrderDetail({ orderItems = [] }) {
+  const totalAmount = orderItems.reduce((acc, item) => acc + item.price, 0);
 
   return (
     <div className="OrderDetail">
       <div className="section-heading">
-        Order Detail
+        <h2>Order Details</h2>
       </div>
       {orderItems.length ? (
         <div>
-          <div>
-            Items:
+          <div className="items-section">
             <ul>
               {orderItems.map((item, idx) => (
-                <li key={idx}>
-                  {item.name} - ${item.price.toFixed(2)}
+                <li key={idx} className="item">
+                  <div className="emoji">{item.emoji}</div>
+                  <div className="item-detail">
+                    <span className="item-name">{item.name}</span>
+                    <span className="item-price">${item.price.toFixed(2)}</span>
+                  </div>
                 </li>
               ))}
             </ul>
           </div>
-          <div>Total Amount: ${totalAmount}</div>
+          <div className="total-section">
+            <h3>Total:</h3>
+            <span>${totalAmount.toFixed(2)}</span>
+          </div>
         </div>
       ) : (
-        <div>No items added yet.</div>
+        <div className="empty-order">
+          Your cart is empty.
+        </div>
       )}
     </div>
   );
--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,22 +1,21 @@
-import {useState, useEffect, useRef} from 'react';
+import { useState, useEffect, useRef } from 'react';
 import * as itemsAPI from '../../utilities/items-api';
 import './NewOrderPage.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Logo from '../../components/Logo/Logo';
 import MenuList from '../../components/MenuList/MenuList';
 import CategoryList from '../../components/CategoryList/CategoryList';
 import OrderDetail from '../../components/OrderDetail/OrderDetail';
-import UserLogOut from '../../components/UserLogOut/UserLogOut';
+
 
 export default function NewOrderPage() {
   const [menuItems, setMenuItems] = useState([]);
   const [activeCat, setActiveCat] = useState([]);
   const [orderItems, setOrderItems] = useState([]);
-  const [user, setUser] = useState([]);
 
   const categoriesRef = useRef([]);
 
-  useEffect(function() {
+  useEffect(function () {
     async function getItems() {
       const items = await itemsAPI.getAll();
       categoriesRef.current = [...new Set(items.map(item => item.category.name))];
@@ -34,23 +33,20 @@ export default function NewOrderPage() {
       <aside className="sidebar">
         <div className="sidebar-top">
           <Logo />
-          <CategoryList
-            categories={categoriesRef.current}
-            activeCat={activeCat}
-            setActiveCat={setActiveCat}
-          />
+          <CategoryList categories={categoriesRef.current} activeCat={activeCat} setActiveCat={setActiveCat} />
           <Link to="/orders" className="button btn-sm">PREVIOUS ORDERS</Link>
-        </div>
-        <div className="sidebar-bottom">
-          <UserLogOut user={user} setUser={setUser} />
         </div>
       </aside>
       <div className="content">
-        <MenuList
-          menuItems={menuItems.filter(item => item.category.name === activeCat)}
-          onAddItemToOrder={handleAddItemToOrder}
+        <div className="menu-list">
+        <MenuList 
+            menuItems={menuItems.filter(item => item.category.name === activeCat)} 
+            onAddItemToOrder={handleAddItemToOrder} 
         />
-        <OrderDetail orderItems={orderItems} />
+        </div>
+        <div className="order-detail">
+          <OrderDetail orderItems={orderItems} />
+        </div>
       </div>
     </main>
   );
